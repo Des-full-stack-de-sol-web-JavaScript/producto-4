04b609a06371830adba7013d11cf61ec3ac1067a
Doplax <doplax@gmail.com>
2025-12-19 11:32:57 +0100

    feat(auth): adapta init_2_login y init_4_usuarios para obtener los datos del backend

--- public/logic/main.js
+++ public/logic/main.js
@@ -1,9 +1,8 @@
 import { headerComponent } from '../components/header.js';
 import { footerComponent } from '../components/footer.js';
-import { almacenaje } from './almacenaje.js'; 
+//import { almacenaje } from './almacenaje.js'; 
 
 document.addEventListener('DOMContentLoaded', () => {
-    almacenaje.initusers();
     headerComponent();
     footerComponent();
 });
